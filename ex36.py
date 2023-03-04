49b17fd9f0541000f25bb8bfa9a16a635bec6f1d
stevengirving <sgirving@outlook.com>
2023-03-04 16:50:41 -0700

    Update psuedo-code layout

--- ex36.py
+++ ex36.py
@@ -2,35 +2,58 @@ from sys import exit
 
 # Adventure design!
 
-prompt = ("> ")
-
-# Start
-def start():
-    print("Hello, and welcome to this game!")
-    player_name = define_player_name()
-    print(f"""
-         Time for an adventure {player_name}!
-         You come across a house while out on a walk.
-         The door is unlocked, open in fact.
-         You could also see if there is anything around the back.
-         Or maybe you should leave?
-
-         > Choose an option
-         > Open door | Go to back | Leave
-         """
-         )
+# Start message welcoming the player
+    # Allow Y|N prompt to exit
 
 # Define player name
-def define_player_name():
-    print("What is your name?")
-    return input(prompt)
+    # Get user input
 
-# Room 1
+# Inventory system?
+    # Honestly probably only like 2-3 items total
+    # Two functions? One to Add, one to Remove?
 
-# Room 2
+# List of completed rooms?
+    # Add room to list when completed to allow backtracking
+    # Each room could check against the list for True, then provide alt description
 
-# Room 3
+# Front of House
+    # -> Enter house
+    # -> Go to back of house
+    # -> Leave
 
+# Back of House
+    # -> Look through windows - 3 choices
+    # -> Go back to front of house
+
+# House Entrance
+    # -> Left Door
+    # -> Right Door
+    # -> Entrance door, now locked
+
+# House Left
+    # Flag unsolved
+    # Some sort of puzzle
+    # Door locked until solved
+    # Secret item?
+    # -> House Back
+    # -> House Entrance
+
+# House Right
+
+# House Back
+
+# House... Secret????
+    # Idea: Collect item from Left and Right to find
+    # Why? For fun!
+
+# Try to Leave
+    # Loop x5 message of walking if loop is False
+    # Return to start
+    # set loop is True
+    # Describe actual exit avoiding loop
+    exit(0)
+
+# Game Over Message
 def dead(why):
     print(f"{why}. Game over!")
     exit(0)
